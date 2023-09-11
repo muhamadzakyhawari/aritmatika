b920efca92f26ce762af721351bb1b93d75652a9
muhamadzakyhawari <144640456+muhamadzakyhawari@users.noreply.github.com>
2023-09-11 09:29:33 +0700

    Create aritmatika.cpp

---
+++ aritmatika.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main( ){
+	int a,b;
+	cout<<"PEMROGRAMAN OPERASI ARITMATIKA\n";
+	cout<<"==================================\n";
+	cout<<"masukan angka pertama: ";
+	cin>>a;
+	cout<<"masukan angka kedua: ";
+	cin>>b;
+	cout<<"hasil penjumlahan: "<<a+b<<endl;
+	cout<<"hasil pengurangan: "<<a-b<<endl;
+    cout<<"hasil perkalian: "<<a*b<<endl;
+	cout<<"hasil pembagian: "<<a/b<<endl;
+	cout<<"hasil modulus: "<<a%b<<endl;
+	return 0;
